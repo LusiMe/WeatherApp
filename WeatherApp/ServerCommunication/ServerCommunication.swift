a6d7d25195d0176c901b9a322efc5af7ef9cbf83
lusime <lusifromrussia@gmail.com>
2022-03-03 14:29:41 +0200

    weather by phone location

---
+++ WeatherApp/ServerCommunication/ServerCommunication.swift
@@ -0,0 +1,92 @@
+//
+//  ServerCommunication.swift
+//  WeatherApp
+//
+//  Created by Mark Parfenov on 01/03/2022.
+//
+
+import Foundation
+
+class ServerCommunication {
+    
+    public static let sharedInstance = ServerCommunication()
+    
+    let API_KEY = "f21454c4f47c7d670854668f10e35e95"
+    
+    let ROOT = "http://api.openweathermap.org/data/2.5/weather?"
+    
+    enum methods {
+        static let get = "GET"
+    }
+    
+    private let weatherDecoder = ResultDecoder<WeatherModel>({ (data) throws -> WeatherModel in
+        try JSONDecoder().decode(WeatherModel.self, from: data)
+    })
+    
+    private func requestBuilder(by coordinates: (Double, Double)) -> URLRequest? {
+        var urlBuilder = URLComponents(string: ROOT)
+        urlBuilder?.queryItems = [
+            URLQueryItem(name: "lat", value: String(coordinates.0)),
+            URLQueryItem(name: "lon", value:  String(coordinates.1)),
+            URLQueryItem(name: "appid", value: API_KEY)
+        ]
+        guard let url = urlBuilder?.url else {return nil}
+        var request = URLRequest(url: url)
+        request.httpMethod = methods.get
+        return request
+    }
+    
+    public func fetchWeather(for coordinates: (Double, Double), onSuccess: @escaping(Result<WeatherModel, NetworkError>) -> Void){
+        if let request = requestBuilder(by: coordinates) {
+            call(request: request, completion: onSuccess)
+        }
+    }
+    
+    private func call(request: URLRequest, completion: @escaping(Result<WeatherModel, NetworkError>) -> Void) {
+    URLSession.shared.dataTask(with: request) { result in
+        completion(self.weatherDecoder.decode(result))
+    }.resume()
+}
+}
+
+// разбить по файлам
+enum NetworkError: Error {
+    case transportError(Error)
+    case serverError(statusCode: Int)
+    case noData
+    case decodingError(Error)
+    case encodingError(Error)
+}
+
+extension NetworkError {
+    init?(data: Data?, response: URLResponse?, error: Error?) {
+        if let error = error {
+            self = .transportError(error)
+            return
+        }
+        
+        if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
+            self = .serverError(statusCode: response.statusCode)
+            return
+        }
+        
+        if data == nil {
+            self = .noData
+        }
+        return nil
+    }
+}
+typealias WeatherResult = Result<Data, NetworkError>
+//typealias WeatherResult = Result<WeatherModel, NetworkError>
+
+extension URLSession {
+    func dataTask(with request: URLRequest, resultHandler: @escaping(WeatherResult) -> Void) -> URLSessionDataTask {
+        return self.dataTask(with: request) { data, responce, error in
+            if let networkError = NetworkError(data: data, response: responce, error: error) {
+                resultHandler(.failure(networkError))
+                return
+            }
+            resultHandler(.success(data!))
+        }
+    }
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var weather: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    var latitude = Double()
+    var longitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        
+        if(CLLocationManager.locationServicesEnabled()) {
+            print("location enabled")
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            locationManager.startMonitoringSignificantLocationChanges()
+        }
     }
-
-
 }
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    //TODO: why called 4 times
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        if let location = locations.last {
+        print("locations", location)
+        latitude = location.coordinate.latitude
+        longitude = location.coordinate.longitude
+      
+        ServerCommunication.sharedInstance.fetchWeather(for: (latitude,longitude), onSuccess: onSuccess)
+    }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("locationManager didFailWithError ", error) //TODO: show to the user
+    }
+    func onSuccess(result: Result<WeatherModel, NetworkError>) {
+        
+        DispatchQueue.main.async {
+            switch result{
+            case .success(let weather):
+                self.weather.text = weather.weather[0].description
+            case .failure(let error):
+                print("on success error", error)
+            }
+        }
+    }
+    }
 

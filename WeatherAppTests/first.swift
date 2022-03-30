cafa72162ba091a3bc273588793ba957ebde711d
lusime <lusifromrussia@gmail.com>
2022-03-30 14:59:35 +0300

    clean up

--- WeatherAppTests/first.swift
+++ WeatherAppTests/first.swift
@@ -67,14 +67,4 @@ class first: XCTestCase {
         XCTAssertEqual(statusCode, 200)
     }
     
-    func testRandomWeatherJsonData() throws {
-//        given
-        var result: Result<WeatherModel, NetworkError>?
-        let weatherData = ["Weather" : "Good"]
-        let urlString = "http://api.openweathermap.org/data/2.5/weather?lat=37.33233141&lon=-122.0312186&appid=f21454c4f47c7d670854668f10e35e95"
-        let url = URL(string: urlString)!
-        let response = try JSONEncoder().encode(weatherData)
-        
-        let session =
-    }
 }
